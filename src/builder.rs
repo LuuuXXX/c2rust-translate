2b6838c254a7b5dc7b85635c7d52cb5713307161
LuuuXXX <luuux98@163.com>
2026-02-05 10:53:39 +0800

    Update builder.rs

--- src/builder.rs
+++ src/builder.rs
@@ -22,7 +22,7 @@ pub fn cargo_build(feature: &str) -> Result<()> {
     
     let output = Command::new("cargo")
         .arg("build")
-        .args(["--message-format", "short"])
+        // .args(["--message-format", "short"])
         .current_dir(&build_dir)
         .output()
         .context("Failed to execute cargo build")?;
